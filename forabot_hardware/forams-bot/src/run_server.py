269e23b76053a859b70be712c369592f45dd3773
vminin2000 <vminin@ncsu.edu>
2023-10-12 19:27:50 -0400

    Fixed folder structure

--- forabot_hardware/forams-bot/src/run_server.py
+++
@@ -1,8 +0,0 @@
-from driver.ForamsSystem import ForamsSystem
-import time
-
-if __name__=='__main__':
-    config_file_location = '/home/pi/Forams/forams-bot/configurations/config_updated.json'
-    #server_config_location = '/home/pi/Forams/forams-bot/configurations/serial_commands.json'
-    foramsTestSystem = ForamsSystem(config_file_location)
-# This runs the foramssystem object
