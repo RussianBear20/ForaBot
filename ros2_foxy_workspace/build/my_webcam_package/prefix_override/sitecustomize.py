86bb947e1aee8787b25186ad2a1667ce82975f2b
vminin2000 <vminin@ncsu.edu>
2023-10-12 20:01:11 -0400

    Added rudimentary webcam node

---
+++ ros2_foxy_workspace/build/my_webcam_package/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/aros/ForaBotResearch/ros2_foxy_workspace/install/my_webcam_package'
---
+++ ros2_foxy_workspace/src/my_webcam_package/my_webcam_package/webcam_node.py
@@ -0,0 +1,35 @@
+import rclpy
+from rclpy.node import Node
+import cv2
+import numpy as np
+
+class WebcamNode(Node):
+
+    def __init__(self):
+        super().__init__('webcam_node')
+        self.timer = self.create_timer(0.1, self.timer_callback)  # 10 Hz
+        self.cap = cv2.VideoCapture(0)
+
+    def timer_callback(self):
+        ret, frame = self.cap.read()
+        if ret:
+            cv2.imshow('Webcam Frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                self.cap.release()
+                cv2.destroyAllWindows()
+                self.destroy_node()
+                rclpy.shutdown()
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = WebcamNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
